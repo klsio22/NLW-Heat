ef2f2c5fe21b0d00a87e50b7cfdf2eb79c9de4a9
klesio25 <klesio.k9@gmail.com>
2021-10-20 10:24:06 -0300

    fix erros and Retrieve user info on github

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,32 +1,44 @@
 import axios from "axios"; //importação via node
 
 /* 
-  Receber code (string)
-  Recuperar o access_tok no github
+  Receber code (string) (yes)
+  Recuperar o access_tok no github (yes)
+  Recuperar o info do usuário no github
   Veruficar se o usario existe no DB
   ---- SIM = Gera um token
   ---- Não = cria no DB , gera um token
   retornar o token com as infos do user
 
 */
+//Criamos uma interface para conseguir ter acesso ao access_token
+interface IAccessTokenResponse {
+  access_token: string
+}
 
 class AuthenticateUserService {
   async execute(code: string) {
-    const url = "https://github.com/login/oauth/access_tokken";
-    const response = await axios.post(url, null, {
+    const url = "https://github.com/login/oauth/access_token";
+    const {data:accessTokenResponse} = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code,
       },
 
+    
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
       },
     });
 
-    return response.data;
+    const response = await axios.get("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
+    })
 
+
+    return response.data;
   }
 }
 
