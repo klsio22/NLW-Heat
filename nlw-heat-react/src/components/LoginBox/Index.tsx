e955b09d40d8b80ddb1448f6c0f882f9014fcb26
klesio25 <klesio.k9@gmail.com>
2021-10-21 14:42:55 -0300

    Finished fist part login box

---
+++ nlw-heat-react/src/components/LoginBox/Index.tsx
@@ -0,0 +1,14 @@
+import { VscGithubInverted } from "react-icons/vsc"
+import styles from "./styles.module.scss";
+
+export function LoginBox() {
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href="" className={styles.singInWithGithub}>
+        <VscGithubInverted size="24" />
+        entrar com GitHub
+      </a>
+    </div>
+  );
+}
