4675e72657642bce54939b5afb90bb1b633b75da
klesio25 <klesio.k9@gmail.com>
2021-10-22 00:09:49 -0300

    fixed errs and get code user authorized

--- nlw-heat-react/src/components/LoginBox/Index.tsx
+++ nlw-heat-react/src/components/LoginBox/Index.tsx
@@ -1,11 +1,27 @@
-import { VscGithubInverted } from "react-icons/vsc"
+import { useEffect } from "react";
+import { VscGithubInverted } from "react-icons/vsc";
 import styles from "./styles.module.scss";
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?Scope=user&client_id=9fdef5059569958d1978`;
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithucode = url.includes("?code=");
+
+    if (hasGithucode) {
+      const [urlWithoutCode, githubCode] = url.split("?code=");
+
+      //console.log({ urlWithoutCode, githubCode });
+      window.history.pushState({}, '', urlWithoutCode);
+    }
+
+  }, []);
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="" className={styles.singInWithGithub}>
+      <a href={signInUrl} className={styles.singInWithGithub}>
         <VscGithubInverted size="24" />
         entrar com GitHub
       </a>
